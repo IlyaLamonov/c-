f2fbd6599fb08dc9ec0e6523c2316123d63512f1
Пользователь <t89527953506@yandex.ru>
2024-01-03 20:42:25 +0300

    домашнее задание

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,25 +1,60 @@
-﻿int a = 6;
-int b = 7;
-int c = 9;
-int d = 4;
-int e = 10;
+﻿//int a = 6;
+//int b = 7;
+//int c = 9;
+//int d = 4;
+//int e = 10;
 
-int max = a;
+//int max = a;
 
-if (b > max)
-{
-    max = b;
-}
-if (c > max)
-{
-    max = c;
-}
-if (d > max)
-{
-    max = d;
-}
-if (e > max)
-{
-    max = e;
-}
-Console.WriteLine(max);
+//if (b > max)
+//{
+//    max = b;
+//}
+//if (c > max)
+//{
+//    max = c;
+//}
+//if (d > max)
+//{
+//    max = d;
+//}
+//if (e > max)
+//{
+//    max = e;
+//}
+//Console.WriteLine(max);
+
+// РЕШЕНИЕ ПЕРВОЙ ЗАДАЧИ НА МАКСИМАЛЬНОЕ И МИНИМАЛЬНОЕ ЧИСЛО
+// int a = 8;
+// int b = 8;
+
+// if (a > b)
+// {
+//     Console.WriteLine("Первое число больше");
+// }
+// else if (a < b)
+// {
+// Console.WriteLine("Второе число больше");
+// }
+// else
+// {
+// Console.WriteLine("Числа равны");
+// }
+
+
+// РЕШЕНИЕ ВТОРОЙ ЗАДАЧИ НА НАХОЖДЕНИ МАКСИСАЛЬНОГО ЧИСЛА ИЗ 3-Х
+
+// int a = 8;
+// int b = 6;
+// int c = 12;
+// int max = a;
+
+// if (b > max)
+// { 
+//     max = b;
+// }
+// if (c > max)
+// { 
+//     max = c;
+// }
+// Console.WriteLine(max);
