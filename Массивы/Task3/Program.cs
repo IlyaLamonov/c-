148721b86a7177721f957c0aeec6080a10096cd0
Пользователь <t89527953506@yandex.ru>
2024-01-09 20:44:54 +0300

    добавил 4-е задачи

---
+++ Массивы/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Пример решения 1
+
+// int n = 10;
+// int[] array = { 2, 5, 38, 46, 82, 3, 4, 8, 6, 9 };
+// int i = 0;
+
+// while(i < n)
+// {
+//     if(array[i] % 2 == 0)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     i = i + 1;
+// }
+
+
+//Пример решения 2
+
+//int n = 10;
+int[] array = { 2, 5, 38, 46, 82, 3, 4, 8, 6, 9 };
+int i = 0;
+
+while(i < array.Length)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write(array[i]);
+        Console.Write(' ');
+    }
+    i = i + 1;
+}
