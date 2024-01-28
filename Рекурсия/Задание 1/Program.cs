5b0e6df3846c02ca9b99d25aa43ba96510c4d212
Пользователь <t89527953506@yandex.ru>
2024-01-28 16:52:22 +0300

    zadanie1

---
+++ Рекурсия/Задание 1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1 - Вычислить фактоориал от натурального числа N
+
+// Этаапы создания рекрсии
+// 1. Рекрсивный вызов
+// 2. Условия остановки рекурсии
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine("Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+
+Console.Write(Fact(5));
